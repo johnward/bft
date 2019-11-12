17861c68aed447a720381de0ec91fdf8f758a072
John Ward <john@johnward.net>
2019-11-12 22:35:06 +0000

    Added commands to the comments

--- bft_interp/src/lib.rs
+++ bft_interp/src/lib.rs
@@ -1,9 +1,21 @@
-//! This is the BrainFuck interpreter
-//!
-//!
 //!
 //! This is a fully working Brainfuck interpretor
 //! =============================================
+//!
+//! Commands
+//! ========
+//! The eight language commands each consist of a single character:
+//!
+//! Character	Meaning
+//!     >	    increment the data pointer (to point to the next cell to the right).
+//!     <	    decrement the data pointer (to point to the next cell to the left).
+//!     +	    increment (increase by one) the byte at the data pointer.
+//!     -	    decrement (decrease by one) the byte at the data pointer.
+//!     .	    output the byte at the data pointer.
+//!     ,	    accept one byte of input, storing its value in the byte at the data pointer.
+//!     [	    if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+//!     ]	    if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+//!             (Alternatively, the ] command may instead be translated as an unconditional jump to the corresponding [ command, or vice versa; programs will behave the same but will run more slowly, due to unnecessary double searching.)
 
 use bft_types::BFCommand;
 use bft_types::InputInstruction;
